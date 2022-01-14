364e19f9270064cff69c5585ab6ef8db2a46f9b9
jonathan earl <jonathan.earl@tylertech.com>
2022-01-14 12:24:48 -0800

    fix refresh blog issue

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/blog',
-        permanent: true,
-      },
-    ]
-  },
+  trailingSlash: true
 }
