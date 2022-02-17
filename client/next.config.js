d2878d0fdf5b765e556f4e73c58606f785cb7660
Jonathan Earl <jonathan.earl@tylertech.com>
2022-02-16 17:35:53 -0800

    chore(deps): update nextjs

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  trailingSlash: true
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
 }
+
+module.exports = nextConfig
