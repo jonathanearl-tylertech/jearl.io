3739a9d196a5186d3cc1ab1b0e190d689330d7ed
Jonathan Earl <jonathan.earl@tylertech.com>
2022-04-09 10:43:04 -0700

    feat: swap from md -> mdx

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = ({ title, back }: { title: string, back: boolean }) => {
   const router = useRouter();
 
   return (
-    <header className={styles.header} onClick={() => router.push('/blog')}>
+    <header className={styles.header} onClick={() => router.push('/')}>
       {back ? <span className={styles.back}>👈</span> : null}
       <span className={styles.title}>{title}</span>
     </header>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import Link from 'next/link';
 
+export default () => {
+  return (<>
+    <Head>
+      <title>Blog | jearl</title>
+      <link rel="icon" href="/favicons/blog/favicon.ico" />
+    </Head>
+    <main>
+      <h1>Jons blog</h1>
+      <section id="intro">
+        <h2>intro</h2>
+        <p>heyo welcome and stay for a while, this is easy listening hour with the power hour of the earl pearl</p>
+      </section>
 
-const Root = () => {
-  const router = useRouter();
-  useEffect(() => { router.push('/blog') }, [router])
-  return (<div>loading</div>)
+      <section id="posts">
+        <h2>posts</h2>
+        <Link href="./post/2022/4/first-post">first post</Link>
+      </section>
+    </main>
+  </>);
 }
-export default Root;
